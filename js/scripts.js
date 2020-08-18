6cf45ac62fdc24a4078c32259beeb4c4aed2ca84
kmansfield27 <36116244+kmansfield27@users.noreply.github.com>
2020-08-17 21:58:49 -0400

    Add function to print employee card to screen

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,44 @@
+const printEmployeeCard = (employee) => {
+    const gallery = document.getElementById('gallery');
+    const card = document.createElement('div');
+
+    card.innerHTML = `<div class="card">
+                        <div class="card-img-container">
+                            <img class="card-img" src="${employee.picture.large}" alt="profile picture">
+                        </div>
+                        <div class="card-info-container">
+                            <h3 id="name" class="card-name cap">${employee.name.first} ${employee.name.last}</h3>
+                            <p class="card-text">${employee.email}</p>
+                            <p class="card-text cap">${employee.location.city}, ${employee.location.state}</p>
+                        </div>
+                    </div>`;
+
+    gallery.appendChild(card);
+}
+
+
+
 const getEmployees = new XMLHttpRequest();
 
-getEmployees.onreadystatechange = function() {
+getEmployees.onreadystatechange = () => {
+
     if (getEmployees.readyState === 4) {
         const response = JSON.parse(getEmployees.responseText);
-        const employees = response.results;   
+        const employees = response.results;
 
         console.log(response);
         console.log(employees);
 
-        for (let i = 0; i< employees.length; i++) {
-            console.log(employees[i].name.first + " " + employees[i].name.last);
-            console.log(employees[i].email);
-            console.log(employees[i].location.city);
-        }
+        employees.forEach( employee => {
+            printEmployeeCard(employee);
+        });
     }
-} 
+}
 
-
-getEmployees.open('GET', 'https://randomuser.me/api/?results=12');
+getEmployees.open('GET', 'https://randomuser.me/api/?results=12&nat=us');
 
 getEmployees.send();
+
+
+
+
